8933062996fe566ac60a6fc147042e157567a82a
cuwuvaaa <ivankolisa@gmail.com>
2024-11-07 09:15:45 +0300

    init

---
+++ siaod_6_2.cpp
@@ -0,0 +1,145 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct alphabet
+{
+	vector <char> used;
+	vector <int> index;
+};
+
+string firstWord(string Text)	//для нахождения первого слова
+{
+	string output;
+	for (char iter : Text)
+	{
+		if (iter == ' ' or iter == ',')
+			return output;
+		output += iter;
+	}
+}
+
+string print_space(int count)
+{
+	string spaces = "";
+	for (int i = 0; i < count; i++)
+		spaces += " ";
+	return spaces;
+}
+
+int index_from_alphabet(char ex, alphabet alph)
+{
+	for (int i = 0; i < alph.used.size(); i++)
+	{
+		if (ex == alph.used[i])
+		{
+			return alph.index[i];
+		}
+	}
+	return alph.index[alph.index.size() - 1];
+}
+
+//БЛОК ДЛЯ СОСТАВЛЕНИЯ ТАБЛИЦЫ ИНДЕКСОВ
+
+int found_count(char example, string ex)
+{
+	int count = 0;
+	for (char iter : ex)
+	{
+		if (example == iter)
+			count++;
+	}
+	return count;
+}
+
+bool found(char example, vector <char> ex)
+{
+	for (char iter : ex)
+	{
+		if (example == iter)
+			return true;
+	}
+	return false;
+}
+
+alphabet table_index(string Word)
+{
+	alphabet ex;
+	if (found_count(Word[Word.length() - 1], Word) == 1)	//если последняя буква уникальна, то присвоение индекса длины - 1
+	{
+		ex.used.push_back(Word[Word.length() - 1]);
+		ex.index.push_back(Word.length() - 1);
+	}
+	for (int i = Word.length() - 2; i >= 1; i--)
+	{
+		if (!found(Word[i], ex.used))
+		{
+			ex.used.push_back(Word[i]);
+			ex.index.push_back(Word.length() - 1 - i);
+		}
+	}
+
+	ex.used.push_back(Word[0]);		//добавляем первую букву слова
+	ex.index.push_back(Word.length() - 1);
+	return ex;
+}
+
+void print_alphabet(alphabet example)
+{
+	for (char iter : example.used)
+	{
+		cout << iter << " ";
+	}
+	cout << endl;
+	for (int iter : example.index)
+	{
+		cout << iter << " ";
+	}
+	cout << endl;
+}
+
+int find_index(string word, string text)
+{
+	int index = 0;
+	int count = 0;
+	bool flag = true;
+	alphabet ex = table_index(word);
+	while (index + word.length() < text.length())
+	{
+		
+		int offset = 0;
+		while (offset < word.length())
+		{
+			if (text[index + word.length() - 1 - offset] == word[word.length() - 1 - offset])
+			{
+				offset++;
+				if (offset == word.length())
+				{
+					count++;
+					index += word.length();
+					cout << print_space(index) << word << "\n";
+					break;
+				}
+			}
+			else if (text[index + word.length() - 1 - offset] != word[word.length() - 1 - offset])
+			{
+				index += index_from_alphabet(text[index + word.length() - 1 - offset],ex);
+				cout << print_space(index) << word << "\n";
+				break;
+			}
+		}
+	}
+	return count;
+}
+
+
+int main()
+{
+	setlocale(0, "");
+	string text = "buffalo, named Buffalo, is from Buffalo, buffalo, other buffaloes from Buffalo.";
+	string first_word = firstWord(text);
+	cout << text << "\n";
+	cout << first_word << "\n";
+	cout << find_index(first_word, text);
+}
---
+++ siaod_6_2_n2.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int hashfunc(string word)
+{
+	int sum = 0;
+	for (int i = 0; i < word.length(); i++ )
+	{
+		sum += word[i] * pow(11, word.length() - i - 1);
+	}
+	return sum;
+}
+
+int RK_func(string text, string sub)
+{
+	string iter = "";
+	int hsub = hashfunc(sub), htext;
+	int start = 0, stop = sub.length();
+	while (start != text.length() - sub.length())
+	{
+		for (int i = start; i < stop; i++)
+		{
+			iter += text[i];
+		}
+		htext = hashfunc(iter);
+		cout << iter << "  " << htext << "\n";
+		if (hsub == htext)
+		{
+			if (iter == sub)
+			{
+				return start;
+			}
+		}
+		start++;
+		stop++;
+		iter = "";
+	}
+	return -1;
+	
+}
+int main()
+{
+	setlocale(0, "");
+	string sub;
+	string text = "There is something incredibly special about the arrival of summer. The air becomes warmer, \n and the days grow longer, filling us with a sense of energy and vitality. \nAs the world awakens from its winter slumber, we can’t help but feel a sense of\n excitement and possibility. One of the best things about summer is the opportunity\n it provides to enjoy the great outdoors.Whether it’s swimming, playing sports, or\n simply soaking up the sun at the beach, summer is the perfect time to get outside and enjoy \nthe beauty of nature. In addition to outdoor activities, summer is also a time for relaxation and\n leisure.It is a time to kick back and enjoy the simple pleasures of life, whether it’s reading a good \nbook, spending time with friends and family, or traveling to \nnew and exciting places.";
+	cout << text << "\n";
+	cout << "Введите образец для нахождения: ";
+	getline(cin,sub);
+	if (RK_func(text, sub) == -1)
+	{
+		cout << "Строка не найдена";
+	}
+	else
+	{
+		cout << "Строка найдена. Индекс первой буквы: " << RK_func(text, sub);
+	}
+}
